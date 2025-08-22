575cc6e274c5afd779d61af153f83e05e447f21c
Ed22mar <edgaricloud@gmail.com>
2025-08-23 03:22:10 +0800

    Finalizando o metodo store

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.main')
-@section('title','Dashboard')
+@section('title','Cadastro')
 @section('content')
 
 <h1>CRIAR UM NOVO POST</h1>
-<form action="/posts" method="post">
+<form action="{{ route('posts.store') }}" method="post">
     @csrf
     <label for="">Title</label>
-    <input type="text" name="" id="">
+    <input type="text" name="title" id="">
 
     <label for="">Body</label>
-    <input class="" type="text" name="" id="">
+    <input class="" type="text" name="body" id="">
 
     <button type="submit">Salvar</button>
 
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[PostController::class, 'index']);
+Route::get('/',[PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/post',[PostController::class,'store'])->name('salvar');
+Route::post('/post',[PostController::class,'store'])->name('posts.store');
